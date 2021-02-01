902b19b5bb153a57929f2344d3030905f387bdab
Arpita Kar <66480315+arpita-sahakar@users.noreply.github.com>
2021-02-01 12:17:31 -0500

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,6 +10,7 @@ import Resume from "./Resume"
 const Main = () => (
     <Switch>
         <Route exact path="/" render={LandingPage}/>
+        <Route exact path="/my-portfolio" render={LandingPage}/>
         <Route path="/aboutme" component={AboutMe}/>
         <Route path="/contact" component={Contact}/>
         <Route path="/projects" component={Projects}/>
